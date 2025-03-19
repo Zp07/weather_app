7ad67bdb852d119888895ba7db76d09065536af1
Zp07 <zepta2249@gmail.com>
2025-03-18 20:34:53 -0500

    feat: mostrar todas las rutas

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,11 @@ from app.schemas import WeatherCreate, WeatherResponse
 
 router = APIRouter()
 
+#Ruta para traer todos los registros
+@router.get("/weather/", response_model=list[WeatherResponse])
+def get_all_weather(db: Session = Depends(get_db)):
+    return db.query(WeatherData).all()
+
 #Ruta para agregar un nuevo registro del clima
 @router.post("/weather/", response_model=WeatherResponse)
 def create_weather(data: WeatherCreate, db: Session = Depends(get_db)):
